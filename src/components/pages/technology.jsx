73afbade0922a8da958cf323f9dbcad6e30ff5d5
AbrahamMehari <abrahammehari0@gmail.com>
2023-10-27 12:34:56 -0500

    added alt to an image

--- src/components/pages/technology.jsx
+++ src/components/pages/technology.jsx
@@ -63,7 +63,7 @@ const LogoGrid = ({ logos}) => {
                     // mobile = xs, laptop = md, monitors = lg, everything larger is xl
                     <Grid item key={logo.label + index} xs={6} sm={4} md={2}>
                         <Card elevation={20} style={cardStyle}>
-                            <img loading='lazy' type="image/svg+xml" src={logo.src} height="50%" width="50%" />
+                            <img alt="" loading='lazy' type="image/svg+xml" src={logo.src} height="50%" width="50%" />
                             <CardContent>
                                 <Typography gutterBottom variant="h5" component="h2" fontFamily="monospace">
                                     {logo.label}
