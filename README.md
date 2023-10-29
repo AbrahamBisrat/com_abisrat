337b25f2f5fc1f3f97a0e4e4547a50fb57fbed3e
AbrahamMehari <abrahammehari0@gmail.com>
2023-10-29 16:11:22 -0500

    updated Readme and provided Lambda code

--- README.md
+++ README.md
@@ -1,41 +1,13 @@
 # Personal Website
 
-Day1: Friday, Oct 20, 2023
+## Hosted on AWS
 
-- Created GH repo
-- Analyzed project spec
-- Setting base React Project
-- Coming up with Design for Front End
+## If you would like the design and want to use it as a template
 
-Day2: Saturday, Oct 21, 2023
+## All data and configs are in the ./src/data Folder.
 
-- Bought Domain and setting up certificates
-- Setting Up DNS configurations
-- React app in progress
+## Every PII is stored there.
 
-Day3: Sunday, Oct 22, 2023
+## For the form submission in Connect section, I used a simple lambda fn with
 
-- Finished Front end with React
-- Some features still ongoing
-- Started Validations
-- Backend started
-- researching Blogs for inspiration
-
-Day4: Monday, Oct 23, 2023
-
-- Set up CloudFront - Done
-- S3 static hosting - Done
-- Route 53 - Configurations - Done
-- Certificates - Done
-
-Day5: Tuesday, Oct 24, 2023
-
-- Implemented backend with Lambda
-- Email Parsing and validation
-- API Gateway
-
-Day6: Wednesday, Oct 25, 2023
-
-- Finishing touches
-
-To Be continued after Final exams . . .
+## Amazon SES to Email it to me. Code can be found on ./src/lambda be sure to disable CORS
---
+++ src/Lambda/lambdaFormSubmitter.js
@@ -0,0 +1,81 @@
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+
+const ses = new SESClient({ region: "us-east-1" });
+
+function generateHTML(payload) {
+  return `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+            }
+
+            .container {
+                max-width: 600px;
+                margin: 0 auto;
+                padding: 20px;
+                background-color: #f9f9f9;
+            }
+            h1 {
+                color: #333;
+            }
+            p {
+                font-size: 16px;
+                line-height: 1.5;
+                color: #666;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Email Form Submission</h1>
+            <h1> </h1>
+            <h2> Here is the data </h2>
+            <br>
+            <p><strong>Email:</strong> ${payload.email}</p>
+            <p><strong>Subject:</strong> ${payload.subject}</p>
+            <p><strong>Message:</strong> ${payload.message}</p>
+        </div>
+    </body>
+    </html>
+  `;
+}
+
+const verifiedEmail = "put_your_email@here.com";
+function generateEmailParams(html) {
+  const params = {
+    Destination: {
+      ToAddresses: [verifiedEmail],
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: html,
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Form Data Submission",
+      },
+    },
+    Source: verifiedEmail,
+  };
+  return params;
+}
+export const handler = async function (event) {
+  const payload = JSON.parse(event.body);
+
+  try {
+    const data = await ses.send(
+      new SendEmailCommand(generateEmailParams(generateHTML(payload)))
+    );
+    return "Email sent successfully";
+  } catch (error) {
+    p(error);
+    // return "Failed to send email";
+    return payload.toString();
+  }
+};
