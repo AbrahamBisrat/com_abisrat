21c8acf5dec64f4be7dd748be0bf1957b27f9a41
Abraham Bisrat <30749258+AbrahamBisrat@users.noreply.github.com>
2026-02-19 11:39:33 -0600

    minor updates

--- src/data/config.js
+++ src/data/config.js
@@ -2,7 +2,7 @@ export const LambdaURL =
   "https://zjtvr3rz2ncske7vyartmlnryi0hpufp.lambda-url.us-east-1.on.aws/";
 
 export const resumeUrl =
-  "https://drive.google.com/file/d/160u0N6fPhCnmDDOfj0MT91YraB0T58-x/view?usp=sharing";
+  "";
 
 export const mediumProfileUrl = 
   "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@abisrat97";
