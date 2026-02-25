9f5ec9660483568718505850b6e049a6a6fccfd5
Abraham Bisrat <30749258+AbrahamBisrat@users.noreply.github.com>
2026-02-25 12:21:13 -0600

    Update resume URL in config.js

--- src/data/config.js
+++ src/data/config.js
@@ -2,7 +2,7 @@ export const LambdaURL =
   "https://zjtvr3rz2ncske7vyartmlnryi0hpufp.lambda-url.us-east-1.on.aws/";
 
 export const resumeUrl =
-  "";
+  "https://drive.google.com/file/d/1wRUCMQ853v1jAmxZyERISZZxCBNtbw0q/view?usp=sharing";
 
 export const mediumProfileUrl = 
   "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@abisrat97";
